ac5628b6f4c21bb21cf36e0e4a5a3f8183ae261a
david810376 <33507197+david810376@users.noreply.github.com>
2020-02-15 18:26:35 -0800

    Create Helper.h

---
+++ Helper.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <SFML\System.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\Graphics.hpp>
+#include <SFML\Audio.hpp>
+#include <SFML\Network.hpp>
+#include <iostream>
+#include <string>
+#include <fstream>
