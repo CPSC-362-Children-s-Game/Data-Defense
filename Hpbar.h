3325aa94107f25eba7ef88dbda4b1a406c4d6e72
david810376 <33507197+david810376@users.noreply.github.com>
2020-02-15 18:22:54 -0800

    Create Hpbar.h

---
+++ Hpbar.h
@@ -0,0 +1,23 @@
+#include"Helper.h"
+
+class sf::RectangleShape;
+
+
+class PlayGui {
+private:
+	
+
+	sf::RectangleShape hpBarback;
+	sf::RectangleShape hpinner;
+  
+	void initHpBar();
+
+public:
+
+void updateHp();
+	
+
+	void renderHpBar(sf::RenderTarget& target);
+	void render(sf::RenderTarget& target);
+
+};
