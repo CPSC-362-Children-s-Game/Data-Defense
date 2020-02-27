16faccc13197ce9eb30d17e8e5dd09b722098183
Richard <riichardphan@gmail.com>
2020-02-26 17:57:20 -0800

    fix player boundaries

--- iteration2/Data Defense/Assets/Scripts/PlayerMovement.cs
+++ iteration2/Data Defense/Assets/Scripts/PlayerMovement.cs
@@ -13,10 +13,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update() {
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal") * speed, 0.0f, 0.0f);
-<<<<<<< HEAD
-
-=======
->>>>>>> 7589c026b1a36b6bcd3eedd6de9885015c7a9cd1
+        
         if ((transform.position.x >= -4.0f && horizontal.x < 0) || (transform.position.x <= 4.0f && horizontal.x > 0)) {
             transform.position = transform.position + horizontal * Time.deltaTime;
         }
