eea06584a22831c2ff0bb72d842aa837a8202f55
rbmauric <35748166+rbmauric@users.noreply.github.com>
2020-03-02 18:36:57 -0800

    added enemy and enemy spawn functionality

---
+++ iteration2/Data Defense/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public float spawnTime = 5f;
+    private Vector2 spawnPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        spawnPosition.x = Random.Range(-3.5f, 3.5f);
+        spawnPosition.y = 3.5f;
+
+        Instantiate(enemy, spawnPosition, Quaternion.identity);
+    }
+
+  
+}
--- iteration2/Data Defense/Assets/Scripts/PlayerMovement.cs
+++ iteration2/Data Defense/Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
-
+   
     // Update is called once per frame
     void Update() {
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal") * speed, 0.0f, 0.0f);
         transform.position = transform.position + horizontal * Time.deltaTime;
+
     }
 }
