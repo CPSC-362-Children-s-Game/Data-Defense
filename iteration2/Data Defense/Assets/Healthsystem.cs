bf29f017af534721296d41ef902487720ee82005
david810376 <33507197+david810376@users.noreply.github.com>
2020-03-08 16:16:46 -0700

    Add files via upload

---
+++ iteration2/Data Defense/Assets/Healthsystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthsystem
+{
+   private:
+   int health;
+   int healthMax;
+
+   public event Eventhandler HealthChanged;
+   public event Eventhandler Dead;
+
+   public Healthsystem(int healthMax)
+   {
+   	this.healthMax = healthMax;
+   	health = healthMax;
+   }
+
+   public float GetHealthpercent(){
+   	return(float)health / healthMax;
+   }
+
+   public void Damage(int amount)
+   {
+   	health -= amount;
+   	if(health<0)
+   	{
+   		health = 0;
+   	}
+   	if(HealthChanged != null) HealthChanged(this, EventArgs.Empty);
+   }
+
+}
