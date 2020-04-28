3ba6de56824c143b24953043a041cb617c825a97
Grace-star <59401683+Grace-star@users.noreply.github.com>
2020-04-28 15:23:00 -0700

    Add files via upload

---
+++ iteration4/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+    //private bool finished = false;
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        // if (finished)
+        //     return;
+
+        float t = Time.time - startTime;
+
+        string minutes = ((int) t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
+    }
+
+    // public void Finish()
+    // {
+    //     finished = true;
+    //     timerText.color = color.yellow;
+    // }
+}
